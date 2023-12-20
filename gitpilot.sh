1bd022cc0b688954a1c8b7aa362d0d90803d962d
Philip <130921787+philipstuessel@users.noreply.github.com>
2023-12-20 21:44:52 +0100

    Add files via upload

---
+++ gitpilot.sh
@@ -0,0 +1,44 @@
+#!/bin/zsh
+gitpilot() {
+    if [[ "$1" == "v" ]]; then
+        echo "${CYAN}GitPilot${NC}"
+        echo "${BOLD}v.0.1.0${NC}"
+        echo "${YELLOW}JAP plugin${NC}"
+    fi
+    
+    if [[ "$1" == "deploying" || "$1" == "dep" ]]; then
+        commit_hash=$(git rev-parse HEAD)
+
+        target_folder="/Users/$USER/gitpilot/$2/"
+        temp_directory="/Users/$USER/gitpilot/$2/temp/"
+
+        mkdir -p "$temp_directory"
+        git archive --format zip --output "$temp_directory.zip" "$commit_hash"
+        unzip "$temp_directory.zip" -d "$temp_directory"
+        cp -r "$temp_directory" "$target_folder"
+        rm -rf "$temp_directory" "$temp_directory.zip"
+        echo "${GREEN}Everything was provided${NC}"
+    fi
+
+    if [[ "$1" == "push" ]]; then
+        if command -v sshpass &> /dev/null; then
+        else
+            echo "sshpass ist nicht installiert. Installiere es mit Homebrew."
+            brew install hudochenkov/sshpass/sshpass
+        fi
+        gitpilot dep "$2"
+        echo "${BLUE}reading gp.json${NC}"
+
+        local_folder="/Users/$USER/gitpilot/$2/"
+        username=$(cat "$(pwd)/gitpilot/gp.json" | grep '"username"' | awk -F ': *' '{print $2}' | tr -d '," ')
+        server_ip=$(cat "$(pwd)/gitpilot/gp.json" | grep '"server_ip"' | awk -F ': *' '{print $2}' | tr -d '," ')
+        target_directory_on_server=$(cat "$(pwd)/gitpilot/gp.json" | grep '"target_directory_on_server"' | awk -F ': *' '{print $2}' | tr -d '," ')
+        ssh_password=$(cat "$(pwd)/gitpilot/gp.json" | grep '"ssh_password"' | awk -F ': *' '{print $2}' | tr -d '," ')
+
+        echo "${BOLD}push on server ...${NC}"
+        sshpass -p "$ssh_password" scp -r "$local_folder"* "$username"@"$server_ip":"$target_directory_on_server"
+        echo "${BLUE}####################################${NC}"
+        echo "${CYAN}GitPilot${NC}${BLUE} has finished its work${NC}"
+        echo "${GREEN}Done${NC}"
+    fi
+}
