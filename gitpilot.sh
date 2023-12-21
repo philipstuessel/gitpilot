d9caf231e8b775e3f488d0f3afec7e61ee0260c3
Philip <130921787+philipstuessel@users.noreply.github.com>
2023-12-21 17:09:34 +0100

    update v0.2.0

--- gitpilot.sh
+++ gitpilot.sh
@@ -2,7 +2,7 @@
 gitpilot() {
     if [[ "$1" == "v" ]]; then
         echo "${CYAN}GitPilot${NC}"
-        echo "${BOLD}v.0.1.0${NC}"
+        echo "${BOLD}v0.2.0${NC}"
         echo "${YELLOW}JAP plugin${NC}"
     fi
     
@@ -12,6 +12,34 @@ gitpilot() {
         target_folder="/Users/$USER/gitpilot/$2/"
         temp_directory="/Users/$USER/gitpilot/$2/temp/"
 
+        if [ -d $target_folder ]; then
+            rm -r $target_folder
+        fi
+
+        mkdir -p "$temp_directory"
+        git checkout -b temp_branch "$commit_hash"
+        changed_files=$(git diff --name-only HEAD^)
+
+        echo "$changed_files" | tar czf "$temp_directory.tar.gz" --files-from -
+        tar xzf "$temp_directory.tar.gz" -C "$temp_directory"
+
+        cp -r "$temp_directory" "$target_folder"
+        rm -rf "$temp_directory" "$temp_directory.tar.gz"
+        git checkout - 
+        git branch -D temp_branch 
+
+        echo "${GREEN}Everything was provided${NC}"
+    fi
+        if [[ "$1" == "deploying-all" || "$1" == "dep-all" ]]; then
+        commit_hash=$(git rev-parse HEAD)
+
+        target_folder="/Users/$USER/gitpilot/$2/"
+        temp_directory="/Users/$USER/gitpilot/$2/temp/"
+        
+        if [ -d $target_folder ]; then
+            rm -r $target_folder
+        fi
+
         mkdir -p "$temp_directory"
         git archive --format zip --output "$temp_directory.zip" "$commit_hash"
         unzip "$temp_directory.zip" -d "$temp_directory"
@@ -20,20 +48,20 @@ gitpilot() {
         echo "${GREEN}Everything was provided${NC}"
     fi
 
-    if [[ "$1" == "push" ]]; then
-        if command -v sshpass &> /dev/null; then
+    if [[ "$1" == "push" || "$1" == "push-all" ]]; then
+        if [[ "$1" == "push-all" ]]; then
+            gitpilot dep-all "$2"
         else
-            echo "sshpass ist nicht installiert. Installiere es mit Homebrew."
-            brew install hudochenkov/sshpass/sshpass
+            gitpilot dep "$2"
+
         fi
-        gitpilot dep "$2"
         echo "${BLUE}reading gp.json${NC}"
 
         local_folder="/Users/$USER/gitpilot/$2/"
-        username=$(cat "$(pwd)/gitpilot/gp.json" | grep '"username"' | awk -F ': *' '{print $2}' | tr -d '," ')
-        server_ip=$(cat "$(pwd)/gitpilot/gp.json" | grep '"server_ip"' | awk -F ': *' '{print $2}' | tr -d '," ')
-        target_directory_on_server=$(cat "$(pwd)/gitpilot/gp.json" | grep '"target_directory_on_server"' | awk -F ': *' '{print $2}' | tr -d '," ')
-        ssh_password=$(cat "$(pwd)/gitpilot/gp.json" | grep '"ssh_password"' | awk -F ': *' '{print $2}' | tr -d '," ')
+        username=$(cat "$(pwd)/.gitpilot/gp.json" | grep '"username"' | awk -F ': *' '{print $2}' | tr -d '," ')
+        server_ip=$(cat "$(pwd)/.gitpilot/gp.json" | grep '"server_ip"' | awk -F ': *' '{print $2}' | tr -d '," ')
+        target_directory_on_server=$(cat "$(pwd)/.gitpilot/gp.json" | grep '"target_directory_on_server"' | awk -F ': *' '{print $2}' | tr -d '," ')
+        ssh_password=$(cat "$(pwd)/.gitpilot/gp.json" | grep '"ssh_password"' | awk -F ': *' '{print $2}' | tr -d '," ')
 
         echo "${BOLD}push on server ...${NC}"
         sshpass -p "$ssh_password" scp -r "$local_folder"* "$username"@"$server_ip":"$target_directory_on_server"
---
+++ update.sh
@@ -0,0 +1,1 @@
+echo $(mkdir -p ~/jap/plugins/packages/ && curl -o ~/jap/plugins/packages/gitpilot.sh https://raw.githubusercontent.com/philipstuessel/gitpilot/main/gitpilot.sh)
